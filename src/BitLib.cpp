cfc94ef5036aa9386bdc5a2e991d511f6672b422
thiminhnhut <thiminhnhut@gmail.com>
2017-11-08 02:25:22 +0700

    Bit Lib Arduino Thu nghiem Update thu vien voi PlatformIO

--- src/BitLib.cpp
+++ src/BitLib.cpp
@@ -10,3 +10,11 @@ BitLib::~BitLib() {
 bool BitLib::getBit(unsigned char number, unsigned char index) {
     return (number >> index) & 0x01;
 }
+
+unsigned char BitLib::getBit(unsigned char number, unsigned char index, unsigned char nobit) {
+    unsigned char value = 0;
+    for (unsigned char i = 0; i < nobit; i++) {
+        value += (number >> (index + i) & 0x01) * (1 << (index + i));
+    }
+    return value;
+}
--- src/BitLib.h
+++ src/BitLib.h
@@ -9,5 +9,6 @@ public:
     ~BitLib();
 
     bool getBit(unsigned char number, unsigned char index);
+    unsigned char getBit(unsigned char number, unsigned char index, unsigned char nobit);
 };
 #endif
