7fdd6e778ff84db2b5b0e17e8f771992e34ffa99
thiminhnhut <thiminhnhut@gmail.com>
2017-11-08 03:35:17 +0700

    Bit Lib Arduino Khac phuc warning trong method getBit2DecChar/Int Define method thay doi trang thai cua mot bit

--- src/BitLib.h
+++ src/BitLib.h
@@ -9,8 +9,13 @@ public:
     ~BitLib();
 
     bool getBit(unsigned char number, unsigned char index);
+    
     unsigned char getBit2DecChar(unsigned char number, unsigned char index, unsigned char nobit);
     unsigned char getBit2DecChar(unsigned int number, unsigned char index, unsigned char nobit);
     unsigned int getBit2DecInt(unsigned int number, unsigned char index, unsigned char nobit);
+
+    unsigned char bitSetChar(unsigned char number, unsigned char index, bool bit);
+    unsigned char bitSetChar(unsigned int number, unsigned char index, bool bit);
+    unsigned int bitSetInt(unsigned int number, unsigned char index, bool bit);
 };
 #endif
