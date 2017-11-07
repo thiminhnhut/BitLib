ac8f124357e875384a1d9132b7497a30aa4f8310
thiminhnhut <thiminhnhut@gmail.com>
2017-11-08 02:53:59 +0700

    Bit Lib Arduino Define method getBit2DecChar/Int

--- src/BitLib.cpp
+++ src/BitLib.cpp
@@ -11,8 +11,24 @@ bool BitLib::getBit(unsigned char number, unsigned char index) {
     return (number >> index) & 0x01;
 }
 
-unsigned char BitLib::getBit2Dec(unsigned int number, unsigned char index, unsigned char nobit) {
+unsigned char BitLib::getBit2DecChar(unsigned char number, unsigned char index, unsigned char nobit) {
     unsigned char value = 0;
+    for (unsigned char i = 0; i < nobit; i++) {
+        value += (number >> (index + i) & 0x01) * (1 << index + i);
+    }
+    return value;
+}
+
+unsigned char BitLib::getBit2DecChar(unsigned int number, unsigned char index, unsigned char nobit) {
+    unsigned char value = 0;
+    for (unsigned char i = 0; i < nobit; i++) {
+        value += (number >> (index + i) & 0x01) * (1 << index + i);
+    }
+    return value;
+}
+
+unsigned int BitLib::getBit2DecInt(unsigned int number, unsigned char index, unsigned char nobit) {
+    unsigned int value = 0;
     for (unsigned char i = 0; i < nobit; i++) {
         value += (number >> (index + i) & 0x01) * (1 << index + i);
     }
--- src/BitLib.h
+++ src/BitLib.h
@@ -9,6 +9,8 @@ public:
     ~BitLib();
 
     bool getBit(unsigned char number, unsigned char index);
-    unsigned char getBit2Dec(unsigned int number, unsigned char index, unsigned char nobit);
+    unsigned char getBit2DecChar(unsigned char number, unsigned char index, unsigned char nobit);
+    unsigned char getBit2DecChar(unsigned int number, unsigned char index, unsigned char nobit);
+    unsigned int getBit2DecInt(unsigned int number, unsigned char index, unsigned char nobit);
 };
 #endif
