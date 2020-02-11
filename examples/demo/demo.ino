50beb762693600c0fcb2c7a804e6f8acf7b48e5f
thiminhnhut <thiminhnhut@gmail.com>
2020-02-11 11:46:33 +0700

    Fix examples

--- examples/demo/demo.ino
+++ examples/demo/demo.ino
@@ -17,61 +17,63 @@ BitLib bit;
  */
 
 void setup() {
-    debug_port.begin(BAUDRATE);
-    debug_port.println("-----------------------------------------------------");
+  debug_port.begin(BAUDRATE);
+  debug_port.println("-----------------------------------------------------");
 
-    unsigned int N = 0xFFFF; // '0b1111 1111 1111 1111'
-    unsigned char count = bit.countBits(N); // 16
-    debug_port.printf("Count Bit of %d: %d\n", N, count);
+  unsigned int N = 0xFFFF;                // '0b1111 1111 1111 1111'
+  unsigned char count = bit.countBits(N); // 16
+  debug_port.printf("Count Bit of %d: %d\n", N, count);
 
-    // 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-    for (unsigned char i = 0; i < count; i++) {
-        debug_port.printf("%d ", bit.getBitIndex(N, i));
-    }
-    debug_port.println("");
+  // 0b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+  debug(N, count);
 
-    unsigned char id = 5;
-    unsigned char math = 7;
-    unsigned char physical = 9;
-    unsigned char chemical = 10;
-    unsigned char enable = 0;
+  unsigned char id = 5;
+  unsigned char math = 7;
+  unsigned char physical = 9;
+  unsigned char chemical = 10;
+  unsigned char enable = 0;
 
-    // 0b1 1111 1111 1111 111 -> 0b1 1111 1111 1111 101
-    N = bit.setBitsIndex(N, 0, 4, id);
+  // 0b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 -> 0b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 1
+  N = bit.setBitsIndex(N, 0, 3, id);
+  debug(N, count);
 
-    // 0b1 1111 1111 1111 101 -> 0b1 1111 1111 0111 101
-    N = bit.setBitsIndex(N, 3, 4, math);
+  // 0b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 1 -> 0b 1 1 1 1 1 1 1 1 1 0 1 1 1 1 0 1
+  N = bit.setBitsIndex(N, 3, 4, math);
+  debug(N, count);
 
-    // 0b1 1111 1111 0111 101 -> 0b1 1111 1001 0111 101
-    N = bit.setBitsIndex(N, 7, 4, physical);
+  // 0b 1 1 1 1 1 1 1 1 1 0 1 1 1 1 0 1 -> 0b 1 1 1 1 1 1 0 0 1 0 1 1 1 1 0 1
+  N = bit.setBitsIndex(N, 7, 4, physical);
+  debug(N, count);
 
-    // 0b1 1111 1001 0111 101 -> 0b1 1010 1001 0111 101
-    N = bit.setBitsIndex(N, 11, 4, chemical);
+  // 0b 1 1 1 1 1 1 0 0 1 0 1 1 1 1 0 1 -> 0b 1 1 0 1 0 1 0 0 1 0 1 1 1 1 0 1
+  N = bit.setBitsIndex(N, 11, 4, chemical);
+  debug(N, count);
 
-    // 0b1 1010 1001 0111 101 -> 0b0 1010 1001 0111 101
-    N = bit.setBitIndex(N, 15, enable);
+  // 0b 1 1 0 1 0 1 0 0 1 0 1 1 1 1 0 1 -> 0b 0 1 0 1 0 1 0 0 1 0 1 1 1 1 0 1
+  N = bit.setBitIndex(N, 15, enable);
+  debug(N, count);
 
-    // 1 0 1 1 1 1 0 1 0 0 1 0 1 0 1 0
-    for (unsigned char i = 0; i < count; i++) {
-        debug_port.printf("%d ", bit.getBitIndex(N, i));
-    }
-    debug_port.println("");
+  id = bit.getBitsIndex2DecChar(N, 0, 3);
+  math = bit.getBitsIndex2DecChar(N, 3, 4);
+  physical = bit.getBitsIndex2DecChar(N, 7, 4);
+  chemical = bit.getBitsIndex2DecChar(N, 11, 4);
+  enable = bit.getBitsIndex2DecChar(N, 15, 1);
+  debug_port.printf("ID:%d, Math:%d, Physical:%d, Chemical:%d, Enable:%d\n", id,
+                    math, physical, chemical, enable);
 
-    id = bit.getBitsIndex2DecChar(N, 0, 3);
-    math = bit.getBitsIndex2DecChar(N, 3, 4);
-    physical = bit.getBitsIndex2DecChar(N, 7, 4);
-    chemical = bit.getBitsIndex2DecChar(N, 11, 4);
-    enable = bit.getBitsIndex2DecChar(N, 15, 1);
-    debug_port.printf("ID:%d, Math:%d, Physical:%d, Chemical:%d, Enable:%d\n",
-                        id, math, physical, chemical, enable);
+  unsigned int value = bit.maxSizeBitsChar(4); // 0b1111 = 15
+  debug_port.printf("value = %d\n", value);
 
-    unsigned int value = bit.maxSizeBitsChar(4); // 0b1111 = 15
-    debug_port.printf("value = %d\n", value);
-
-    unsigned int value2 = bit.maxSizeBitsInt(16); // 0xFF = 65535
-    debug_port.printf("value = %d\n", value2);
+  unsigned int value2 = bit.maxSizeBitsInt(16); // 0xFF = 65535
+  debug_port.printf("value = %d\n", value2);
 }
 
-void loop() {
+void loop() {}
 
+void debug(unsigned int N, unsigned char count) {
+  debug_port.printf("N=%d:\t", N);
+  for (int i = count - 1; i >= 0; i--) {
+    debug_port.printf("%d ", bit.getBitIndex(N, i));
+  }
+  debug_port.println("");
 }
